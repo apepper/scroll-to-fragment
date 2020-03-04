e4afa3c01462a562c96fabe2b8f6f969ed8daf9e
David Császár <dcsaszar@users.noreply.github.com>
2020-03-04 18:01:17 +0100

    Initial implementation

---
+++ src/index.ts
@@ -0,0 +1,135 @@
+import * as History from "history";
+
+interface Options {
+  getElement?: (fragmentId: string) => Element | undefined;
+  history?: History.History;
+  observeDomMutation?: boolean;
+  observeTimeoutMs?: number;
+  scrollIntoView?: (element: Element) => void;
+  scrollOnAnchorClick?: boolean;
+  scrollOnStart?: boolean;
+  stopOnInteraction?: boolean;
+}
+
+export function scrollToFragment(options: Options = {}) {
+  unmount();
+
+  currentOptions = {
+    getElement: options.getElement ?? getElementById,
+    history: options.history,
+    observeDomMutation: options.observeDomMutation ?? true,
+    observeTimeoutMs: options.observeTimeoutMs ?? 7000,
+    scrollIntoView: options.scrollIntoView ?? scrollIntoView,
+    scrollOnAnchorClick: options.scrollOnAnchorClick ?? true,
+    scrollOnStart: options.scrollOnStart ?? true,
+    stopOnInteraction: options.stopOnInteraction ?? true
+  };
+
+  mount();
+}
+
+function mount() {
+  if (currentOptions.observeDomMutation) {
+    documentObserver = new MutationObserver(handleDomMutation);
+  }
+  if (currentOptions.scrollOnAnchorClick) {
+    document.addEventListener("click", handleDocumentClick);
+  }
+  unlistenHistory = currentOptions.history?.listen(handleHistoryPush);
+  if (currentOptions.scrollOnStart) startObserving();
+}
+
+function unmount() {
+  stopObserving();
+  document.removeEventListener("click", handleDocumentClick);
+  if (unlistenHistory) unlistenHistory();
+  unlistenHistory = undefined;
+  documentObserver = undefined;
+}
+
+function startObserving() {
+  stopObserving();
+  if (!location.hash) return;
+
+  // console.log(`SSTH 👍 start ${location.href}`);
+
+  if (currentOptions.stopOnInteraction) {
+    addEventListener("touchend", handleInteraction);
+    addEventListener("wheel", handleInteraction);
+    document.addEventListener("selectionchange", handleInteraction);
+  }
+  documentObserver?.observe(document, OBSERVER_CONFIG);
+  adjustScrollPosition();
+
+  observeTimeout = setTimeout(stopObserving, currentOptions.observeTimeoutMs);
+}
+
+function stopObserving() {
+  // console.log("SSTH ✋stop");
+
+  clearTimeout(observeTimeout);
+  cancelAnimationFrame(throttleRequestId);
+  documentObserver?.disconnect();
+  removeEventListener("touchend", handleInteraction);
+  removeEventListener("wheel", handleInteraction);
+  document.removeEventListener("selectionchange", handleInteraction);
+}
+
+function handleHistoryPush(
+  _location: History.Location<{}>,
+  action: History.Action
+) {
+  // console.log(`SSTH 🏟 history: ${action} ${location.href}`);
+  if (action === "PUSH") startObserving();
+}
+
+function handleDocumentClick(event: Event) {
+  if ((event.target as Node).nodeName === "A") {
+    // console.log("SSTH 🔗 click");
+    throttle(startObserving);
+  }
+}
+
+function handleDomMutation() {
+  // console.log("SSTH 🦕 mutation");
+  throttle(adjustScrollPosition);
+}
+
+function handleInteraction() {
+  // console.log("SSTH 💨interaction");
+  stopObserving();
+}
+
+function adjustScrollPosition() {
+  if (!location.hash) return;
+  const fragmentId = decodeURIComponent(location.hash.substring(1));
+  const element = currentOptions.getElement.call(null, fragmentId);
+  if (element) currentOptions.scrollIntoView.call(null, element);
+}
+
+function getElementById(id: string) {
+  return document.getElementById(id);
+}
+
+function scrollIntoView(element: Element) {
+  element.scrollIntoView();
+  // console.log("SSTH 🏎 show");
+}
+
+function throttle(callback: () => void) {
+  cancelAnimationFrame(throttleRequestId);
+  throttleRequestId = requestAnimationFrame(callback);
+}
+
+let currentOptions: Readonly<Options>;
+let unlistenHistory: History.UnregisterCallback | undefined;
+let documentObserver: MutationObserver | undefined;
+let observeTimeout: number | undefined;
+let throttleRequestId: number | undefined;
+
+const OBSERVER_CONFIG = {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true
+};
